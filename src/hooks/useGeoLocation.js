8e59a6d544521ae438d3bec5a374576d6ea0c43b
naval7871 <navalsunar7871@gmail.com>
2023-11-28 11:43:53 +0530

    fixed setInterval bug in useGeolocation.js hook

--- src/hooks/useGeoLocation.js
+++ src/hooks/useGeoLocation.js
@@ -7,6 +7,7 @@ export function useGeoLocation() {
   });
 
   useEffect(() => {
+    let retry = 1;
     const intervel = setInterval(() => {
       if (!("geolocation" in navigator)) {
         console.log("Geolocation is not supported");
@@ -14,31 +15,41 @@ export function useGeoLocation() {
           code: 0,
           message: "geolocation not supported",
         });
+        clearInterval(intervel);
         return;
       }
 
-      const onSuccess = (location) => {
-        //console.log("Location fetched successfully:", location);
-        setLocation({
-          loaded: true,
-          coordinates: {
-            lat: location.coords.latitude,
-            lon: location.coords.longitude,
-          },
-        });
-      };
+      if(retry === 3) {
+        clearInterval(intervel);
+      }
 
-      const onError = (error) => {
-        console.error("Error getting geolocation:", error);
-        setLocation((prevState) => ({
-          ...prevState,
-          loaded: false,
-          error,
-        }));
-      };
-
+      retry = retry + 1;
       navigator.geolocation.getCurrentPosition(onSuccess, onError);
     });
+
+    function onSuccess (location){
+      //console.log("Location fetched successfully:", location);
+      setLocation({
+        loaded: true,
+        coordinates: {
+          lat: location.coords.latitude,
+          lon: location.coords.longitude,
+        },
+      });
+      clearInterval(intervel);
+    };
+
+    function onError(error){
+      console.error("Error getting geolocation:", error);
+      setLocation((prevState) => ({
+        ...prevState,
+        loaded: false,
+        error,
+      }));
+      clearInterval(intervel);
+    };
+
+
     return () => clearInterval(intervel);
   }, []); // Empty dependency array to run this effect only once
 
